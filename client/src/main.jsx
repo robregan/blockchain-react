7ba6d2288c2d860da11793250031befaa6856a57
rob <robert_regan86@yahoo.com>
2022-02-06 19:48:04 -0500

    transaction cards done with dummy data and giphy api setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import TransactionProvider from './context/TransactionContext'
+import { TransactionsProvider} from './context/TransactionContext'
 
 ReactDOM.render(
- <TransactionProvider>
+ <TransactionsProvider>
     <App />
-    </TransactionProvider>,
+    </TransactionsProvider>,
   document.getElementById('root')
 )
